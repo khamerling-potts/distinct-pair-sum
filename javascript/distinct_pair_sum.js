b72cbbfccabc2af1b4f71c59b7d0090bd273a705
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-11-02 13:54:11 -0400

    fixed for consecutive numbers

--- javascript/distinct_pair_sum.js
+++ javascript/distinct_pair_sum.js
@@ -1,17 +1,15 @@
 function distinctPairSum(arr, k) {
-  const result = [];
   const seen = {};
-  for (let i = 0; i < arr.length; i++) {
+  for (let i = 0; i < arr.length - 1; i++) {
     const element = arr[i];
     const pair = k - element;
     if (element in seen || pair in seen) {
       continue;
-    } else if (arr.includes(pair) && arr.indexOf(pair) !== i) {
-      seen[element] = pair;
-      result.push([element, pair]);
+    } else if (arr[i + 1] === pair) {
+      seen[element] = [element, pair];
     }
   }
-  return result;
+  return Object.values(seen);
 }
 
 if (require.main === module) {
@@ -25,7 +23,9 @@ if (require.main === module) {
   console.log("=>", distinctPairSum([3, 4, 2, 1, 5, 2, 8, 2], 10));
 
   console.log("expecting [[2, 7], [6, 3], [-1, 10], [4.5, 4.5]]");
-  console.log(distinctPairSum([3, 5, 8, 4.5, 6, 7, 6, 4.5, 2, -1, 10], 9));
+  console.log(
+    distinctPairSum([3, 6, 5, 8, 4.5, 4.5, 6, 2, 7, 6, 2, -1, 10], 9)
+  );
 }
 
 module.exports = distinctPairSum;
@@ -33,15 +33,13 @@ module.exports = distinctPairSum;
 // Please add your pseudocode to this file
 // And a written explanation of your solution
 /*
-result = [[], []]
 seen = {}
 loop through the array
   const element = arr[i]
   const pair = k - element
   if element or pair in seen
    continue
-  else if pair in array and its not the current element
-    seen[element] = pair;
-    result.push(element, pair])
-return result
+  else if next element equals pair
+    seen[element] = [element, pair];
+return seen's values
 */
