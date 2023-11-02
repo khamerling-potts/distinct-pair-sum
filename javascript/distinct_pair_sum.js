9513c8e35402c34fe4bd51cd35cf9eb68d66ab3c
Kyra Hamerling-Potts <khamerling-potts@wustl.edu>
2023-11-02 13:46:11 -0400

    done

--- javascript/distinct_pair_sum.js
+++ javascript/distinct_pair_sum.js
@@ -1,5 +1,17 @@
 function distinctPairSum(arr, k) {
-  // type your code here
+  const result = [];
+  const seen = {};
+  for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+    const pair = k - element;
+    if (element in seen || pair in seen) {
+      continue;
+    } else if (arr.includes(pair) && arr.indexOf(pair) !== i) {
+      seen[element] = pair;
+      result.push([element, pair]);
+    }
+  }
+  return result;
 }
 
 if (require.main === module) {
@@ -11,9 +23,25 @@ if (require.main === module) {
 
   console.log("Expecting: [[2, 8]]");
   console.log("=>", distinctPairSum([3, 4, 2, 1, 5, 2, 8, 2], 10));
+
+  console.log("expecting [[2, 7], [6, 3], [-1, 10], [4.5, 4.5]]");
+  console.log(distinctPairSum([3, 5, 8, 4.5, 6, 7, 6, 4.5, 2, -1, 10], 9));
 }
 
 module.exports = distinctPairSum;
 
 // Please add your pseudocode to this file
 // And a written explanation of your solution
+/*
+result = [[], []]
+seen = {}
+loop through the array
+  const element = arr[i]
+  const pair = k - element
+  if element or pair in seen
+   continue
+  else if pair in array and its not the current element
+    seen[element] = pair;
+    result.push(element, pair])
+return result
+*/
